297522bc17a96a8e2b0da80d3d329f76bccc298a
ggottemo <ggottemo717@gmail.com>
2023-05-24 11:14:56 -0400

    Web Api module

---
+++ WebAPI/Controllers/PizzaController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : Controller
+{
+    public PizzaController()
+    {
+        
+    }
+   
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+        PizzaService.GetAll();
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
+
+    // POST 
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
+
+    // PUT
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+            return BadRequest();
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+
+    // DELETE
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
+}
